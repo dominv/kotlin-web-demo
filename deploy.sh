2b020c95314eb51f4ee82c665494921f480df3d0
domlopez <domlopez@starbucks.com>
2018-06-04 20:32:33 +0200

    Fixing travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo service docker start
+sudo apt-get update
+
+sudo apt-get install openjdk-8-jdk
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
