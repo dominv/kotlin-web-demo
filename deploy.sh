ae27169e3bda098c1264b81a4f356b78f2608df8
domlopez <domlopez@starbucks.com>
2018-06-04 20:29:36 +0200

    Fixing travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo service docker start
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
