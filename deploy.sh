815b19d56b4614d6c18a8830041dd68e9fe7c8d9
domlopez <domlopez@starbucks.com>
2018-06-04 20:58:35 +0200

    Fixing travis.yml

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ sudo cp ./kotlin.web.demo.server/build/libs/WebDemoWar.war ./docker/frontend/war
 
 sudo cp ./kotlin.web.demo.backend/build/libs/WebDemoBackend.war ./docker/backend/war/WebDemoBackend.war
 
-docker-compose build
+sudo docker-compose build
 
-docker-compose up
+sudo docker-compose up
