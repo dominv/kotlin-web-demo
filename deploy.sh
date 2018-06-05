a749342c723678b9b134e432f48dde8120f7fdc6
domlopez <domlopez@starbucks.com>
2018-06-05 17:21:57 +0200

    deployment test

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 sudo apt-get update
 
-sudo service mysql stop
-
 sudo apt-get -y install openjdk-8-jdk
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
