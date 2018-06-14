d7a874267a18c10db4ba2c0f0e0f49a540697681
domlopez <domlopez@starbucks.com>
2018-06-15 01:10:25 +0200

    deployment test

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-
-sudo apt-get -y install openjdk-8-jdk
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose version
-
-sh gradlew
-
-sh gradlew ::copyKotlinLibs
-
 sh gradlew war
-
-mkdir ./docker/frontend/war/
-
-mkdir ./docker/backend/war/
 
 sudo cp ./kotlin.web.demo.server/build/libs/WebDemoWar.war ./docker/frontend/war/WebDemoWar.war
 
 sudo cp ./kotlin.web.demo.backend/build/libs/WebDemoBackend.war ./docker/backend/war/WebDemoBackend.war
+
+sudo docker-compose build
+
+sudo docker-compose up -d
