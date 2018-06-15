a12f6f8620836f293adbdc927494cbab3fa7a533
domlopez <domlopez@starbucks.com>
2018-06-15 06:43:22 +0200

    deployment test

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-sudo docker-compose down
+git checkout master
+
+git pull
 
 sh gradlew war
 
 sudo cp ./kotlin.web.demo.server/build/libs/WebDemoWar.war ./docker/frontend/war/WebDemoWar.war
 
 sudo cp ./kotlin.web.demo.backend/build/libs/WebDemoBackend.war ./docker/backend/war/WebDemoBackend.war
+
+sudo docker-compose down
 
 sudo docker-compose build
 
