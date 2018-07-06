607b468f9f0d815b32b975fb3369586bde907c2a
domlopez <domlopez@starbucks.com>
2018-07-06 18:18:55 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -3,15 +3,15 @@
 # Pull latest version of published subtree
 git checkout master
 git subtree add --prefix .trykotlin https://github.com/dominv/kotlin-web-demo.git master --squash
-git subtree pull --prefix .trykotlin --message="[skip ci] Update subtree" https://github.com/dominv/kotlin-web-demo.git master --squash
+git subtree pull --prefix .trykotlin --message="Update subtree" https://github.com/dominv/kotlin-web-demo.git master --squash
 
 # Update arrow version
 echo 0.7.4 > .trykotlin/arrowktversion
 git add .trykotlin/arrowktversion
-git commit -m "[skip ci] Upgrading arrow version"
+git commit -m "Upgrading arrow version"
 
 # Push built subtree to trykotlinwebdemo
 git subtree push --prefix .trykotlin https://github.com/dominv/kotlin-web-demo master master
 rm -rf .trykotlin
 git add .trykotlin/
-git commit -m "[skip ci] Cleaning trykotlin"
+git commit -m "Cleaning trykotlin"
