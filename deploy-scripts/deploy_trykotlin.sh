c89c26780d9dd8d8a656a93f0e79232f2a6118d3
domlopez <domlopez@starbucks.com>
2018-07-06 17:18:37 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -2,10 +2,9 @@
 
 # git remote add trykotlin https://github.com/dominv/kotlin-web-demo
 git fetch trykotlin
-git pull trykotlin
 git checkout trykotlin/master -- arrowktversion
 
 cat arrowktversion
-echo 0.7.3 >> arrowktversion
+echo 0.7.3 > arrowktversion
 git commit -m "Upgraded arrow version in trykotlin"
 git push
