fc556aeec08bde66b807feca2495207906e3fb16
domlopez <domlopez@starbucks.com>
2018-07-06 19:22:25 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -6,8 +6,12 @@ git subtree add --prefix .trykotlin https://github.com/dominv/kotlin-web-demo.gi
 git subtree pull --prefix .trykotlin --message="Update trykotlin subtree" https://github.com/dominv/kotlin-web-demo.git master --squash
 
 # Update arrow version
-echo 0.7.4 > .trykotlin/arrowktversion
-git add .trykotlin/arrowktversion
+echo arrowKtVersion=0.7.4 >> .trykotlin/versions/1.0.7/gradle.properties
+echo arrowKtVersion=0.7.4 >> .trykotlin/versions/1.1.60/gradle.properties
+echo arrowKtVersion=0.7.4 >> .trykotlin/versions/1.2.31/gradle.properties
+git add .trykotlin/versions/1.0.7/gradle.properties
+git add .trykotlin/versions/1.1.60/gradle.properties
+git add .trykotlin/versions/1.2.31/gradle.properties
 git commit -m "Upgrading arrow version in trykolin"
 
 # Push built subtree to trykotlinwebdemo
