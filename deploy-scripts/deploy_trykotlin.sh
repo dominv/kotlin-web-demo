31d93ba298b12fd1a09cf304575e671ee091cb81
domlopez <domlopez@starbucks.com>
2018-07-06 14:16:57 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -2,11 +2,10 @@
 
 git remote add trykotlin https://github.com/dominv/kotlin-web-demo
 git fetch trykotlin
-git checkout -b trykotlin-update-arrow-version trykotlin/master
-git checkout master
-git read-tree --prefix=trykotlin/arrow -u trykotlin-update-arrow-version
-cd arrow
-ls
+git checkout trykotlin-update-arrow-version trykotlin/master -- arrow/arrowktversion
+
+# git checkout master
+#git read-tree --prefix=trykotlin/arrow -u trykotlin-update-arrow-version
 cat arrowktversion
 git commit -m "Merged library project as subdirectory"
 git push
