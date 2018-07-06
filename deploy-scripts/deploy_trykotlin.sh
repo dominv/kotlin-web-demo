c3b40825ce00b25684ac7488545f33f8ce1e796a
domlopez <domlopez@starbucks.com>
2018-07-06 18:22:04 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -11,7 +11,7 @@ git add .trykotlin/arrowktversion
 git commit -m "Upgrading arrow version"
 
 # Push built subtree to trykotlinwebdemo
-git subtree push --prefix .trykotlin https://github.com/dominv/kotlin-web-demo master master
+git subtree push --prefix .trykotlin https://github.com/dominv/kotlin-web-demo.git master
 rm -rf .trykotlin
 git add .trykotlin/
 git commit -m "Cleaning trykotlin"
