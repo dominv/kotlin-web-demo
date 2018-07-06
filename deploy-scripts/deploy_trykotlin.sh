6355eea6735364939261f9580ecdc33bdb9d8df4
domlopez <domlopez@starbucks.com>
2018-07-06 17:51:08 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -3,4 +3,9 @@
 git remote add trykotlin https://github.com/dominv/kotlin-web-demo
 git fetch trykotlin
 git checkout -b trykotlin-update-arrow-version trykotlin/master
-git read-tree --prefix=trykotlin/arrow -u trykotlin-update-arrow-version
+git checkout master
+git read-tree --prefix=trykotlin -u trykotlin-update-arrow-version
+git checkout trykotlin-update-arrow-version
+git pull
+git checkout master
+git merge --squash -s subtree --no-commit trykotlin-update-arrow-version
