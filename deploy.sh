57517d2d8d17071471154946c9cc67e61416e73c
domlopez <domlopez@starbucks.com>
2018-07-08 22:03:30 +0200

    Squashed '.trykotlin/' content from commit 46bea736e
    git-subtree-dir: .trykotlin
    git-subtree-split: 46bea736e28a654efbf939d0cb557a9eb85ec53c

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export ARROW_VERSION=$(cat arrowktversion)
+
+git checkout master
+
+git pull
+
+sh gradlew war
+
+sudo cp ./kotlin.web.demo.server/build/libs/WebDemoWar.war ./docker/frontend/war/WebDemoWar.war
+
+sudo cp ./kotlin.web.demo.backend/build/libs/WebDemoBackend.war ./docker/backend/war/WebDemoBackend.war
+
+sudo docker-compose down
+
+sudo docker-compose build
+
+sudo docker-compose up -d
---
+++ kotlin.web.demo.frontend/src/providers/CompletionProvider.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package providers
+import model.Project
+import utils.codemirror.Position
+import utils.jquery.JQuery
+import views.ActionStatusMessage
+import kotlin.js.json
+
+class CompletionProvider(
+        var onSuccess: () -> Unit,
+        var onFail: (error: String, status: ActionStatusMessage) -> Unit
+)
+{
+    private var isLoadingCompletion = false
+    private var request: JQuery.Promise? = null
+
+    fun getCompletion(
+            project: Project,
+            filename: String,
+            cursor: Position,
+            callback: (Array<CompletionProposal>) -> Unit
+    ) {
+        if (isLoadingCompletion) {
+            request?.done(callback)
+        }
+
+        isLoadingCompletion = true
+        request = ajax(url = generateAjaxUrl(REQUEST_TYPE.COMPLETE, hashMapOf("runConf" to project.confType)),
+                dataType = DataType.JSON,
+                timeout = 10000,
+                type = HTTPRequestType.POST,
+                data = json(
+                        "project" to JSON.stringify(project),
+                        "filename" to filename,
+                        "line" to cursor.line,
+                        "ch" to cursor.ch
+                ),
+                success = { completionProposals: Array<CompletionProposal> ->
+                    isLoadingCompletion = false
+                    onSuccess()
+                    callback(completionProposals)
+                },
+                error =  { jqXHR: dynamic, textStatus: String, errorThrown: String ->
+                    isLoadingCompletion = false
+                    if (jqXHR.responseText != null && jqXHR.responseText != "") {
+                        onFail(jqXHR.responseText, ActionStatusMessage.get_completion_fail)
+                    } else {
+                        onFail(textStatus + " : " + errorThrown, ActionStatusMessage.get_completion_fail)
+                    }
+                }
+        )
+    }
+}
+
+
+public data class CompletionProposal(val icon: String, val text: String, val displayText: String, val tail: String)
+
