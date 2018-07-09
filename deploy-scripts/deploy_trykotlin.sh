2715c26511a9ce99be2d7a46ae7c465beec13be1
domlopez <domlopez@starbucks.com>
2018-07-09 12:05:03 +0200

    deploy

--- deploy-scripts/deploy_trykotlin.sh
+++ deploy-scripts/deploy_trykotlin.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 # Add and pull latest version of trykotlin subtree
-git subtree add --prefix .trykotlin https://github.com/dominv/kotlin-web-demo.git master --squash
-git subtree split --prefix .trykotlin -b trykotlin
+#git subtree add --prefix .trykotlin https://github.com/dominv/kotlin-web-demo.git master --squash
+#git subtree split --prefix .trykotlin -b trykotlin
 #git subtree pull --prefix .trykotlin --message="Update trykotlin subtree" https://github.com/dominv/kotlin-web-demo.git master --squash
+
+git remote add trykotlin https://github.com/dominv/kotlin-web-demo.git
+git branch trykotlin_master trykotlin/master
+
+git checkout -f trykotlin_master
+git subtree split --squash --prefix arrowkt -b trykotlin_temp
 
 # Update arrow version
 echo $VERSION_NAME > .trykotlin/arrowktversion
